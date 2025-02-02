1f2b45f11468c75b4dd26257163fa46d95bf4764
taemath <hirotaka081630@gmail.com>
2025-02-02 14:47:00 +0900

    feat(p.40) add /article path

--- main.go
+++ main.go
@@ -11,7 +11,12 @@ func main() {
 		io.WriteString(w, "Hello, World\n")
 	}
 
+	postingArticleHandler := func(w http.ResponseWriter, req *http.Request) {
+		io.WriteString(w, "Posting article...\n")
+	}
+
 	http.HandleFunc("/hello", helloHandler)
+	http.HandleFunc("/article", postingArticleHandler)
 
 	log.Println("server started at :8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
