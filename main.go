359a827b81587200467400e3ed51c60a4b4fd882
taemath <hirotaka081630@gmail.com>
2025-02-02 14:52:47 +0900

    prac(p.41) 実践演習

--- main.go
+++ main.go
@@ -15,8 +15,28 @@ func main() {
 		io.WriteString(w, "Posting article...\n")
 	}
 
+	articleListHandler := func(w http.ResponseWriter, req *http.Request) {
+		io.WriteString(w, "Article List\n")
+	}
+
+	articleShowHandler := func(w http.ResponseWriter, req *http.Request) {
+		io.WriteString(w, "Article No.1\n")
+	}
+
+	postingNiceHandler := func(w http.ResponseWriter, req *http.Request) {
+		io.WriteString(w, "Posting Nice...\n")
+	}
+
+	postingCommentHandler := func(w http.ResponseWriter, req *http.Request) {
+		io.WriteString(w, "Posting Comment...\n")
+	}
+
 	http.HandleFunc("/hello", helloHandler)
 	http.HandleFunc("/article", postingArticleHandler)
+	http.HandleFunc("/article/list", articleListHandler)
+	http.HandleFunc("/article/1", articleShowHandler)
+	http.HandleFunc("/article/nice", postingNiceHandler)
+	http.HandleFunc("/article/comment", postingCommentHandler)
 
 	log.Println("server started at :8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
